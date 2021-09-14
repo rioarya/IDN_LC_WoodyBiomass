3fcfcc97f6d1d79742698ba12efebe7c55fbc493
Rio Aryapratama <aryapratama.rio@gmail.com>
2021-09-14 07:17:41 +0200

    Add files via upload

---
+++ 1. Main Results in the Manuscript/(IMPORTANT) readme1.py
@@ -0,0 +1,8 @@
+This folder is dedicated to the Python script to run the improved harvesting conversion efficiency scenarios (results shown in the manuscript)
+
+- Each python script accompanied by the corresponding Excel Files. Some parts of the script will read the data in Excel files.
+- Please check further guideline/workflow in Appendix A, Table A.22.
+- The flow names in Excel files may not so intuitive. I invite the readers to check the Appendix C for a more tidy version of the 
+yearly material flow tables. There, you can also find out the corresponding flow codes that can be linked with the system definition (Fig. 1)
+- Some scenarios like PF_PO, PF_FP and DL_FP has counter use material flow in separate excel files (name started with 'NonRW...')
+- Please replace 'C:\\my_path\\scenario_files.xlsx' with your own path/directory. 
