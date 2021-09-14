1ba0992af2c1b56c453488f1de93a4c985357d0c
Rio Aryapratama <aryapratama.rio@gmail.com>
2021-09-14 07:21:03 +0200

    Add files via upload

---
+++ 5. Test Cases, Appendix A/(IMPORTANT) readme5.py
@@ -0,0 +1,10 @@
+This folder is dedicated to the Python script to run the test case in Appendix A, section A.3.1
+
+- Each python script accompanied by the corresponding Excel Files. Some parts of the script will read the data in Excel files.
+- Please check further guideline/workflow in Appendix A, Table A.22.
+- The 'PF_SF_test_energy' script corresponds to the test case in Fig. A.3 in Appendix A. 
+- The 'PF_SF_test_energy' script will read the tab 'PF_SF_E_trial' in 'PF_SF.xlsx'.
+- The 'PF_SF_test_mat' script corresponds to test cases in Fig. A.4 - A.6 in Appendix A
+- The 'PF_SF_test_mat' test case script will read the tab 'PF_SF_S1_mat' in 'PF_SF.xlsx'. The users can modify the fraction of HWP that is stored
+in the technosphere in column E2 in 'PF_SF_S1_mat'. Currently it shows 1, which means 100% of HWP is stored in the technosphere.
+- Please replace 'C:\\my_path\\scenario_files.xlsx' with your own path/directory. 
