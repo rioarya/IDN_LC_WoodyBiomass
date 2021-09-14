413b16243b44ca8de0d3a8046e1f206dbd298956
Rio Aryapratama <aryapratama.rio@gmail.com>
2021-09-14 07:18:06 +0200

    Delete (IMPORTANT) readme2.py

--- 2. Existing Conversion Efficiency (EC), Appendix A/(IMPORTANT) readme2.py
+++
@@ -1,9 +0,0 @@
-This folder is dedicated to the Python script to run the existing conversion efficiency scenarios (results only shown in Appendix)
-
-- Each python script accompanied by the corresponding Excel Files. Some parts of the script will read the data in those Excel files.
-- The flow names in Excel files may not so intuitive. I invite the readers to check the Appendix C for a more tidy version of the 
-yearly material flow tables. There, you can also find out the corresponding flow codes that can be linked with the system definition (Fig. 1)
-- Some scenarios like PF_PO_EC, PF_FP_EC and DL_FP_EC has counter use material flow in separate excel files (name started with 'NonRW...')
-- PF_FP and DL_FP Excel files contains the data for both improved (main manuscript) and existing conversion efficiency scenarios (Appendix).
-They are the same excel files as those in 'Main Results in the Manuscript' folder and don't have the 'EC' remark in their names.
-
